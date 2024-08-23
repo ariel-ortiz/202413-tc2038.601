d96f97d3979188ef42805efef228311a33aff019
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2024-08-23 11:03:30 -0600

    Add code for bin_with_num_bits

--- 3. Bitwise Instructions/bit_fun.py
+++ 3. Bitwise Instructions/bit_fun.py
@@ -21,3 +21,21 @@ def int_mul(m: int, n: int) -> int:
         m >>= 1
         n <<= 1
     return -result if negative else result
+
+
+def int_log2(n: int) -> int:
+    if n <= 0:
+        raise ValueError(f'int_log2 not defined for {n}')
+    bit_count: int = 0
+    while n:
+        bit_count += 1
+        n >>= 1
+    return bit_count - 1
+
+
+def bin_with_num_bits(n: int, b: int) -> str:
+    if b < 1 or n >= (1 << b) or n < -(1 << (b - 1)):
+        raise ValueError(f"Can't fit {n} into {b} bits")
+    if n < 0:
+        n += (1 << b)
+    return f'{n:0{b}b}'
--- 3. Bitwise Instructions/test_bit_fun.py
+++ 3. Bitwise Instructions/test_bit_fun.py
@@ -1,5 +1,5 @@
 import unittest
-from bit_fun import is_even, is_power_of_2, int_mul
+from bit_fun import is_even, is_power_of_2, int_mul, bin_with_num_bits
 
 class TestIsEven(unittest.TestCase):
     def test_even_number(self):
@@ -58,6 +58,32 @@ class TestIsEven(unittest.TestCase):
         self.assertEqual(int_mul(7, 0), 0)
         self.assertEqual(int_mul(0, 0), 0)
         self.assertEqual(int_mul(666, 0), 0)
+
+    def test_positive_numbers_bin_with_num_bits(self):
+        self.assertEqual(bin_with_num_bits(5, 4), '0101')
+        self.assertEqual(bin_with_num_bits(3, 3), '011')
+        self.assertEqual(bin_with_num_bits(8, 4), '1000')
+
+    def test_negative_numbers_bin_with_num_bits(self):
+        self.assertEqual(bin_with_num_bits(-1, 4), '1111')
+        self.assertEqual(bin_with_num_bits(-3, 4), '1101')
+        self.assertEqual(bin_with_num_bits(-5, 16), '1111111111111011')
+
+    def test_zero_bin_with_num_bits(self):
+        self.assertEqual(bin_with_num_bits(0, 4), '0000')
+        self.assertEqual(bin_with_num_bits(0, 1), '0')
+
+    def test_max_min_values_bin_with_num_bits(self):
+        self.assertEqual(bin_with_num_bits(7, 4), '0111')
+        self.assertEqual(bin_with_num_bits(-8, 4), '1000')
+
+    def test_value_error_bin_with_num_bits(self):
+        with self.assertRaises(ValueError):
+            bin_with_num_bits(16, 4)
+        with self.assertRaises(ValueError):
+            bin_with_num_bits(-9, 4)
+        with self.assertRaises(ValueError):
+            bin_with_num_bits(1, 0)
 
 if __name__ == '__main__':
     unittest.main()
