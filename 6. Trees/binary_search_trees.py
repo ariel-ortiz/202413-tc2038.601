932bc309ed6fd765351e1a43af473b047b1662fa
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2024-09-27 09:50:36 -0600

    Add code for BST traversal: post_order and level order

--- 6. Trees/binary_search_trees.py
+++ 6. Trees/binary_search_trees.py
@@ -1,7 +1,8 @@
-from typing import Any, Iterator, Optional
+from typing import Any, Iterator
+from collections import deque
 
 
-type Tree = Optional[list[Any]]
+type Tree = list[Any] | None
 
 
 def in_order(root: Tree) -> Iterator[Any]:
@@ -20,15 +21,42 @@ def pre_order(root: Tree) -> Iterator[Any]:
     yield from pre_order(root[2])
 
 
+def post_order(root: Tree) -> Iterator[Any]:
+    if not root:
+        return
+    yield from post_order(root[1])
+    yield from post_order(root[2])
+    yield root[0]
+
+
+def level_order(root: Tree) -> Iterator[Any]:
+    queue: deque[Tree] = deque()
+    queue.append(root)
+    while queue:
+        current: Tree = queue.popleft()
+        if current:
+            value, left, right = current
+            yield value
+            queue.append(left)
+            queue.append(right)
+
+
 if __name__ == '__main__':
     tree: Tree = [5,
                   [3,
                    [1, None, None],
-                      [4, None, None]],
+                   [4, None, None]],
                   [8,
-                      None,
-                      [10,
-                       [9, None, None],
-                          None]]]
+                   None,
+                   [10,
+                    [9, None, None],
+                     None]]]
     print(list(in_order(tree)))
     print(list(pre_order(tree)))
+    print(list(post_order(tree)))
+    print(list(level_order(tree)))
+
+    for elem in level_order(tree):
+        if elem % 2 == 0:
+            print(f'Found { elem }')
+            break
